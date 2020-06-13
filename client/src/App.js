f695f187d9f9266210a40f5496aa40499ae78735
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-13 09:55:30 +0530

    Add input fields in 'Join'

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Join from './Join/Join';
 import Chat from './Chat/Chat';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Join />
-        <Chat />
+        {/* <Chat /> */}
       </div>
     );
   }
--- client/src/Join/Join.jsx
+++ client/src/Join/Join.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const Join = () => <h1>Join</h1>;
+const Join = () => (
+  <div>
+    <div className="mx-auto mt-5" style={{ width: '30%' }}>
+      <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          placeholder="Enter name"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          name="room"
+          placeholder="Enter room"
+        />
+      </div>
+      <button className="btn btn-primary btn-block">Join</button>
+    </div>
+  </div>
+);
 
 export default Join;
