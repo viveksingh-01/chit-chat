86375b53ca5535bf239b9b10445f5945175a3f10
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-14 15:47:59 +0530

    Add router file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const PORT = process.env.PORT || 5000;
+const router = require('./router');
 
 const app = express();
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
