0a7cc590384f2a14da330898fe23f4b357f1c930
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-15 01:52:27 +0530

    Use hooks to store user's name and room in 'Chat'

--- client/src/Chat/Chat.jsx
+++ client/src/Chat/Chat.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
 
-const Chat = () => <h1>Chat</h1>;
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    setName(name);
+    setRoom(room);
+  });
+
+  return null;
+};
 
 export default Chat;
