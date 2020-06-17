a08c8f13c170ceeb5e4ac3bbffcee7927e414079
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-17 22:33:28 +0530

    Emit disconnect event and remove event listener on hook unmount

--- client/src/Chat/Chat.jsx
+++ client/src/Chat/Chat.jsx
@@ -15,6 +15,11 @@ const Chat = ({ location }) => {
 
     const socket = io.connect(ENDPOINT);
     socket.emit('join', { name, room });
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    };
   }, [ENDPOINT, location.search]);
 
   return null;
