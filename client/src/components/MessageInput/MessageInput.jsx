1b6a8db3fd9f17e19ae43fef87a7cec1e85a2218
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-22 22:59:18 +0530

    Modify styles for MessageInput

--- client/src/components/MessageInput/MessageInput.jsx
+++ client/src/components/MessageInput/MessageInput.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import './MessageInput.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const MessageInput = () => {
   return (
     <div className="d-flex align-items-center my-3">
       <input
         type="text"
-        className="form-control mr-3"
+        className="form-control input-message"
         placeholder="Enter message"
       />
-      <button type="submit" className="btn btn-primary">
-        Send
+      <button type="submit" className="btn btn-primary btn-send">
+        <FontAwesomeIcon icon={faPaperPlane} />
+        <span className="mx-2">Send</span>
       </button>
     </div>
   );
