56e5fac6a007f9d18371730893bc8ae7fb587d66
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-22 18:15:47 +0530

    Move all the components to components folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Join from './Join/Join';
-import Chat from './Chat/Chat';
-import Navbar from './navbar/Navbar';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends React.Component {
   render() {
