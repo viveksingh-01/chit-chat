4e3709ad7261436417c7e855511def5e7751638d
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-23 23:10:21 +0530

    Update MessageInput

--- client/src/components/MessageInput/MessageInput.jsx
+++ client/src/components/MessageInput/MessageInput.jsx
@@ -1,37 +1,35 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import './MessageInput.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import { MessagesContext } from '../../_contexts/MessagesContext';
 
 const MessageInput = ({ sendMessage }) => {
   const [message, setMessage] = useState('');
-  const setMessages = useContext(MessagesContext)[1];
 
   const onSubmit = (evt) => {
     evt.preventDefault();
-    console.log(message);
     sendMessage(message);
-    // setMessages((messages) => [...messages, message]);
   };
 
   return (
-    <div className="d-flex align-items-center my-3">
-      <input
-        type="text"
-        className="form-control input-message"
-        placeholder="Enter message"
-        onChange={(evt) => setMessage(evt.target.value)}
-      />
-      <button
-        type="submit"
-        className="btn btn-primary btn-send"
-        onClick={(e) => onSubmit(e)}
-      >
-        <FontAwesomeIcon icon={faPaperPlane} />
-        <span className="mx-2">Send</span>
-      </button>
-    </div>
+    <form onSubmit={(e) => onSubmit(e)}>
+      <div className="d-flex align-items-center my-3">
+        <input
+          type="text"
+          className="form-control input-message"
+          placeholder="Enter message"
+          onChange={(evt) => setMessage(evt.target.value)}
+        />
+        <button
+          type="submit"
+          className="btn btn-primary btn-send"
+          onClick={(e) => onSubmit(e)}
+        >
+          <FontAwesomeIcon icon={faPaperPlane} />
+          <span className="d-none d-md-inline mx-md-2">Send</span>
+        </button>
+      </div>
+    </form>
   );
 };
 
