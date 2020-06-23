ab4907a1db1798224811adf4eaf1e5fd197b9c09
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-24 01:43:11 +0530

    Update 'UserList'

--- client/src/components/UserList/UserList.jsx
+++ client/src/components/UserList/UserList.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './UserList.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 const UserList = ({ users }) => {
   return (
     <div className="container-user-list">
       <h6>IN ROOM</h6>
-      {users ? users.map(({ name, id }) => <li key={id}>{name}</li>) : null}
+      {users
+        ? users.map(({ name, id }) => (
+            <div key={id} className="mb-1">
+              <FontAwesomeIcon
+                icon={faCircle}
+                style={{ color: '#00ff00', fontSize: '12px' }}
+              />
+              <span className="ml-2 text-capitalize">{name}</span>
+            </div>
+          ))
+        : null}
     </div>
   );
 };
