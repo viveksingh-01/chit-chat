711fbccb17cc398e995fac987c877e7953c7bd5b
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-24 02:00:14 +0530

    Add emoji support for the message

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './Message.css';
+import ReactEmoji from 'react-emoji';
 
 const Message = ({ message: { message, user }, name }) => {
   const isSent = user === name ? true : false;
   return isSent ? (
     <div className="d-flex justify-content-end">
       <div className="container-msg msg-sent">
-        <span className="text-muted username">{user}</span>
-        <p className="message">{message}</p>
+        <span className="text-muted username">You</span>
+        <p className="message">{ReactEmoji.emojify(message)}</p>
       </div>
     </div>
   ) : (
     <div className="d-flex justify-content-start">
       <div className="container-msg msg-received">
         <span className="text-muted username">{user}</span>
-        <p className="message">{message}</p>
+        <p className="message">{ReactEmoji.emojify(message)}</p>
       </div>
     </div>
   );
